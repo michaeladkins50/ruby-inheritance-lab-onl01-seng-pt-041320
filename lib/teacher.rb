f61a5df08b6c580cdcf722ae558acd3909b313a6
Michael Adkins <mikeadkins1112@gmail.com>
2020-05-15 00:00:49 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,9 +1,10 @@
 class Teacher < User
   attr_accessor :first_name, :last_name
+  attr_reader :knowledge
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
   def learn
-    KNOWLEDGE.sample
+    @knowledge = KNOWLEDGE.sample
   end
   
 end
